585ab95f9af72bd24773bd63c21cbc72170cbf97
JesperBaltzersen <jesperbaltzersen@gmail.com>
2025-04-03 22:18:56 +0200

    add workshop winners

---
+++ workshops/incident_management/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.btn').addEventListener('click', () => {
+        alert('Photo upload feature coming soon!');
+    });
+
+    document.querySelector('.btn.secondary').addEventListener('click', () => {
+        alert('Redirecting to all workshops page...');
+    });
+});
